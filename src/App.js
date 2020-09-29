936c65c42935f0ea753031f0b5fc7f5f6553d239
shivam <thuglifegaming321@gmail.com>
2020-09-30 02:07:24 +0530

    Product Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from './Header'
+import Home from './Home.js'
+
 import './App.css';
 
 function App() {
@@ -7,6 +9,7 @@ function App() {
     <div className="App">
       {/*<h1>Jai Shri Ram</h1>*/}
       <Header />
+      <Home />
     </div>
   );
 }
